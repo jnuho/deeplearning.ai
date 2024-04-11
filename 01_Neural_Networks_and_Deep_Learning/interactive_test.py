e4b9daa0f196b9e3dc83cf159fd747d1fd7dd8f2
jnuho <cactoos555@gmail.com>
2024-04-11 11:55:21 +0900

    .

--- 01_Neural_Networks_and_Deep_Learning/interactive_test.py
+++ 01_Neural_Networks_and_Deep_Learning/interactive_test.py
@@ -1,25 +1,88 @@
 # Package imports
 # %%
 import numpy as np
+import h5py
 import matplotlib.pyplot as plt
 from testCases_v2 import *
-from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
 
-from datetime import datetime
 # %matplotlib inline
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# %load_ext autoreload
+# %autoreload 2
+
+np.random.seed(1)
+
+def relu(Z):
+  A = np.maximum(0,Z)
+  cache = Z
+
+  return A, cache
+
+def sigmoid(Z):
+  A = 1/(1+np.exp(-Z))
+  cache = Z
+
+  return A, cache
+
+def linear_forward(A_prev, W,b):
+  Z = np.dot(W,A_prev) + b
+  cache = (A_prev, W, b)
+
+  return Z, cache
+
+def linear_activation_forward(A_prev, W, b, activation):
+
+  if activation=="relu":
+    Z, linear_cache = linear_forward(A_prev, W, b)
+    A, activation_cache = relu(Z)
+  elif activation=="sigmoid":
+    Z, linear_cache = linear_forward(A_prev, W, b)
+    A, activation_cache = relu(Z)
+
+  cache = (linear_cache, activation_cache)
+
+  return A, cache
+
+def init_parameters_deep(layer_dims):
+  L = len(layer_dims)
+  parameters = {}
+
+  # l = 1,2,... L-1
+  for l in range(1, L):
+    parameters["W"+str(l)]= np.random.randn(layer_dims[l], layer_dims[l-1])*.01
+    parameters["b"+str(l)]= np.zeros((layer_dims[l],1))
+
+  return parameters
+  
+def L_model_forward(X, parameters):
+  A = X
+  caches = []
+  L = len(parameters) // 2
+  for l in range(1,L):
+    A_prev = A
+    A, cache = linear_activation_forward(A_prev,
+                                         parameters["W"+str(l)],
+                                         parameters["b"+str(l)],
+                                         activation="relu"
+                                         )
+    caches.append(cache)
+  
+  AL, cache = linear_activation_forward(A,
+                                         parameters["W"+str(L)],
+                                         parameters["b"+str(L)],
+                                         activation="sigmoid")
+  caches.append(cache)
+
+  return AL, caches
 
 
-# %%
-# X, Y = load_planar_dataset()
-# Visualize the data:
-# plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral);
+if __name__=="__main__":
+  layer_dims = [5,3,1]
+  # Wi, bi for i=1,...,L-1
+  parameters = init_parameters_deep(layer_dims)
 
-np.random.seed(1) # set a seed so that the results are consistent: for grading programming exercise
-
-n_x = 100
-X1 = np.random.randn(5,3)
-print(X1)
-
-mean=0
-std_dev= 1
-X2 = np.random.normal(mean, std_dev, size=(5, 3))
+  print(parameters)
--- test.py
+++ test.py
@@ -1,18 +1,47 @@
 import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+# from testCases_v2 import *
+# from dnn_utils_v2 import sigmoid, sigmoid_backward, relu, relu_backward
 
-# dim: dimension
-def initialize_with_zeroes(dim):
-  """
-  w: vector of shape (dim,1)
-  b: 0 (float or int)
-  """
-  w = np.zeros(shape=(dim,1), dtype=np.float32)
-  b = .0
-  return w, b
+# %matplotlib inline
+plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+plt.show()
 
-if __name__ == "__main__":
-  
-  w,b = initialize_with_zeroes(3)
-  print(w)
-  print(b)
+# %load_ext autoreload
+# %autoreload 2
 
+
+np.random.seed(1)
+
+
+def L_model_forward(X, parameters):
+  A = X
+  cache = []
+
+  AL = None
+
+  return AL, cache
+
+
+
+if __name__=="__main__":
+  X = np.random.randn(1,3)
+  print(X)
+
+# Turn on interactive mode
+plt.ion()
+
+# Create a simple plot
+x = np.linspace(0, 2*np.pi, 400)
+y = np.sin(x**2)
+
+# A figure with just one sub-plot
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_title('A single plot')
+
+# Show the plot (in interactive mode, this should not be necessary)
+plt.show()
